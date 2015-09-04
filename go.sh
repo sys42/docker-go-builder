f54d359674b0dd050fed13de429817f0e59f94c1
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-09-04 19:02:54 +0200

    handle environment variables GOOS, GOARCH, CGO_ENABLED and TRACE

--- go.sh
+++ go.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-docker run -ti --rm -v $PWD:/app -w /app $(cat REPO_AND_VERSION) \
+
+TMPENV=""
+[ -n "$GOOS" ]        && TMPENV="-e GOOS=$GOOS"
+[ -n "$GOARCH" ]      && TMPENV="$TMPENV -e GOARCH=$GOARCH"
+[ -n "$CGO_ENABLED" ] && TMPENV="$TMPENV -e CGO_ENABLED=$CGO_ENABLED"
+[ -n "$TRACE" ]       && TMPENV="$TMPENV -e TRACE=$TRACE"
+
+[ -n "$TRACE" ] && echo "TMPENV = [$TMPENV]"
+
+docker run -ti --rm -v $PWD:/app -w /app $TMPENV $(cat REPO_AND_VERSION) \
        remapuser app $(id -u) $(id -g) goexec "$@"
