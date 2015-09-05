cee959211da40bb92e8c85f52371aa5c7c82cfe0
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-09-05 12:05:45 +0200

    combine lines checking for $TRACE

--- go.sh
+++ go.sh
@@ -6,9 +6,7 @@ TMPENV=""
 [ -n "$GOOS" ]        && TMPENV="-e GOOS=$GOOS"
 [ -n "$GOARCH" ]      && TMPENV="$TMPENV -e GOARCH=$GOARCH"
 [ -n "$CGO_ENABLED" ] && TMPENV="$TMPENV -e CGO_ENABLED=$CGO_ENABLED"
-[ -n "$TRACE" ]       && TMPENV="$TMPENV -e TRACE=$TRACE"
-
-[ -n "$TRACE" ] && echo "TMPENV = [$TMPENV]"
+[ -n "$TRACE" ]       && TMPENV="$TMPENV -e TRACE=$TRACE" && echo "TMPENV = [$TMPENV]"
 
 
 docker run -ti --rm -v $PWD:/app -w /app $TMPENV "$IMAGE_TO_USE" \
